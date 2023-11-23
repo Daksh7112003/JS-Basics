638fc3f87bace8f8c24d481f3c431dcfbaecb348
Daksh7112003 <13anil67@gmail.com>
2023-11-23 17:32:21 +0530

    DAY1

---
+++ DAY1/index.js
@@ -0,0 +1,20 @@
+console.log("Hello world ji");
+
+
+let  lastName = 'Babbar';
+console.log(lastName);
+lastName=3;
+console.log(lastName);
+
+
+
+
+console.log(3-2);
+
+console.log(2**3);
+
+let age=27;
+let status =(age>=18)? 'Yes Vote': 'No Vote';
+console.log(status);
+
+
