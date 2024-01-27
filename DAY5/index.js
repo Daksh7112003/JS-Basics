6001a6fa7e15510d25382cfcc41444c30d61d362
Daksh7112003 <13anil67@gmail.com>
2024-01-28 01:34:39 +0530

    index.js

---
+++ DAY5/index.js
@@ -0,0 +1,9 @@
+let obj = {
+    item:"pen",
+    price:10,
+    
+
+};
+let output = 'the cost of ${obj.item} is ${obj.price} rupees';
+console.log(output);
+console.log("the cost of"  , obj.item, "is ", obj.price , "rupees");
