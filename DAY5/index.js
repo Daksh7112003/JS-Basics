d497ade719aab2a32661b0521ef209e8a1f80856
Daksh7112003 <13anil67@gmail.com>
2024-01-28 22:16:28 +0530

    hii

--- DAY5/index.js
+++ DAY5/index.js
@@ -4,6 +4,16 @@ let obj = {
     
 
 };
-let output = 'the cost of ${obj.item} is ${obj.price} rupees';
-console.log(output);
+// let output = 'the cost of ${obj.item} is ${obj.price} rupees';
+// console.log(output);
 console.log("the cost of"  , obj.item, "is ", obj.price , "rupees");
+
+
+//template literals in js..
+
+
+console.log("Apna\n college");
+
+
+let str ="apna\tcollege"
+console.log(str.length);
